0a0ebe17e50f476b94c002073fac68eb85d86051
Cade Ritter <ritter.cade@gmail.com>
2018-04-05 06:02:55 -0500

    first commit

---
+++ js/riceapps.js
@@ -0,0 +1,12 @@
+feather = require('feather.min.js');
+
+$(function() {
+  feather.replace();
+  var currentTab = location.pathname;
+  $('.nav-item').each(function() {
+    var navlink = $(this).find('.nav-link');
+    if (navlink.attr('href').indexOf(currentTab) !== -1) {
+      $(this).addClass('active');
+    }
+  })
+});
