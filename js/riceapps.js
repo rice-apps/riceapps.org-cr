2f3780e66b9d1d8a7a4483e3f53c75022ae196dd
Cade Ritter <ritter.cade@gmail.com>
2018-04-08 22:43:24 -0500

    ui updates

--- js/riceapps.js
+++ js/riceapps.js
@@ -1,9 +1,9 @@
-$(function() {
-  var currentTab = location.pathname;
-  $('.nav-item').each(function() {
-    var navlink = $(this).find('.nav-link');
-    if (navlink.attr('href').indexOf(currentTab) !== -1) {
-      $(this).addClass('active');
-    }
-  })
-});
+// $(function() {
+//   var currentTab = location.pathname;
+//   $('.nav-item').each(function() {
+//     var navlink = $(this).find('.nav-link');
+//     if (navlink.attr('href').indexOf(currentTab) !== -1) {
+//       $(this).addClass('active');
+//     }
+//   })
+// });
